d27e820045d0da7e2eff3b07f5c5e8f6f1242d2f
BugHunter-X <askzenai@gmail.com>
2025-01-16 14:46:04 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,10 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+function addString(a: string, b: string): string {
+  return a + b;
+}
+
+let result1 = add(5, 10); // Correct
+let result2 = addString('5', '10'); // Correct for string addition
